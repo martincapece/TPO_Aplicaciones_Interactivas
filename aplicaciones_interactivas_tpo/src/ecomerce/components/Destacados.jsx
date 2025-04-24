fe2d009bc279f90942d74073616ca54b4f1029d5
Martin Capece <macapece@uade.edu.ar>
2025-04-24 18:03:40 -0300

    feat(destacados): implementacion de mapeo de sneakers basadas en 'dataDestacados.js'

--- aplicaciones_interactivas_tpo/src/ecomerce/components/Destacados.jsx
+++ aplicaciones_interactivas_tpo/src/ecomerce/components/Destacados.jsx
@@ -1,6 +1,8 @@
-import { Typography } from "@mui/material"
+import { Box, Card, CardContent, CardMedia, Grid, Typography } from "@mui/material"
+import { dataDestacados } from '../data/dataDestacados'
 
 export const Destacados = () => {
+
     return (
         <>
             <Typography 
@@ -9,11 +11,76 @@ export const Destacados = () => {
                 fontSize: '45px',
                 fontFamily: 'Inter',
                 fontWeight: 800,
-                mt: 10
+                my: 10
             }}
             >
                 DESTACADOS
             </Typography>
+            
+            <Grid 
+            container
+            spacing={ 2 }
+            direction="row"
+            >
+            {
+                dataDestacados.map( ({ id, price, model, brand, colors, image }) => (
+                    <Grid
+                    key={ id }
+                    size={{ xs: 12, sm: 6, md: 4, lg: 3 }}
+                    >
+                        <Card>
+                            <CardMedia
+                                component="img"
+                                alt={ model }
+                                // height="20px"
+                                image={ image }
+                            />
+                            <CardContent
+                            sx={{
+                                height: '200px',
+                            }}
+                            >
+                                <Box
+                                    sx={{
+                                        display: 'inline-flex',
+                                        alignItems: 'center',
+                                        justifyContent: 'center',
+                                        backgroundColor: '#f65454',
+                                        border: '1px solid #ff7e7e',
+                                        padding: '2px 6px',
+                                        mb: 2
+                                    }}
+                                    >
+                                    <Typography
+                                        sx={{
+                                        color: '#ffffff',
+                                        fontFamily: 'Inter',
+                                        fontWeight: 700,
+                                        fontStyle: 'italic',
+                                        fontSize: 12,
+                                        }}
+                                    >
+                                        NEW IN
+                                    </Typography>
+                                </Box>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    ${price}
+                                </Typography>
+                                <Typography gutterBottom variant="h6" component="div">
+                                    {model}
+                                </Typography>
+                                <Typography variant="h6" sx={{ color: '#C0C0C0' }}>
+                                    {brand}
+                                </Typography>
+                                <Typography variant="h6" sx={{ color: '#C0C0C0' }}>
+                                    {colors.length} colores
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                ))
+            }
+            </Grid>
             
             {/* TRAER DATA PARA MOSTRAR DESTACADOS => habria que mapearlos (data.map( () => {} )) */}
         </>
