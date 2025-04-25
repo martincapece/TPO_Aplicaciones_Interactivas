0690fac990194409f157de2bdbea93b3b69ef036
simon forteza <fortezasimon@gmail.com>
2025-04-25 12:08:32 -0300

    feature(Cart): renderizado condicional para cuando no hay productos en el carrito

--- aplicaciones_interactivas_tpo/src/Cart/components/Cart.jsx
+++ aplicaciones_interactivas_tpo/src/Cart/components/Cart.jsx
@@ -2,16 +2,37 @@ import './cart.css'
 import CartItemList from './CartItemList';
 import PurchaseDetail from './PurchaseDetail';
 import { useCart } from '../hooks/useCart';
+import { useNavigate } from 'react-router-dom'; // Importamos useNavigate
 
 function Cart() {
-    const {productList, subtotal} = useCart()
-    
+    const { productList, subtotal } = useCart()
+    const navigate = useNavigate()
+
+    const isEmpty = productList.length === 0;
+
+    const handleBuyClick = () => {
+        navigate('/') //redirige a la pagina principal
+    }
+
     return (
         <div className="cart-container">
-            <CartItemList productList={productList} />
-            <PurchaseDetail subtotal={subtotal} />
+            {
+                isEmpty
+                    ? (
+                        <div className="empty-cart">
+                            <h1>Carrito vacío</h1>
+                            <button className="buy-button" onClick={handleBuyClick}>Comprar</button>
+                        </div>
+                    )
+                    : (
+                        <>
+                            <CartItemList productList={productList} />
+                            <PurchaseDetail subtotal={subtotal} />
+                        </>
+                    )
+            }
         </div>
-    )
+    );
 }
 
 
