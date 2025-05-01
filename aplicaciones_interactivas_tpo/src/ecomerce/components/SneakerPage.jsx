3330cdee5b4a5a5cef57b13c42483243dfe6e329
Martin Capece <macapece@uade.edu.ar>
2025-05-01 02:23:58 -0300

    feat(sneaker-page): cambios en el diseño y responisve implementado

--- aplicaciones_interactivas_tpo/src/ecomerce/components/SneakerPage.jsx
+++ aplicaciones_interactivas_tpo/src/ecomerce/components/SneakerPage.jsx
@@ -7,7 +7,6 @@ import { useCart } from "../../Cart/hooks/useCart";
 import { Link } from "react-router-dom";
 import { Accordion, AccordionSummary, AccordionDetails } from "@mui/material";
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import { motion } from "framer-motion";
 import { Dialog, DialogTitle, DialogContent, DialogActions } from "@mui/material";
 
 export default function SneakerPage() {
@@ -23,61 +22,56 @@ export default function SneakerPage() {
     }
 
 return (
-    <motion.div 
-        initial={{ opacity: 0, x: 30 }}
-        animate={{ opacity: 1, x: 0 }}
-        exit={{ opacity: 0, x: -30 }}
-        transition={{ duration: 0.5, ease: "easeInOut" }}
+    <Grid
+    container
+    sx={{
+        margin: '0 auto',
+        maxWidth: { xs: '300px', sm: '600px', md: '800px', lg: '1000px' },
+    }}
     >
-        <Box p={4}>
-            <Grid container spacing={4}>
-            {/* Imagen y miniaturas */}
-            <Grid item xs={12} md={6}>
-                <Stack direction="row" spacing={1}>
+        <Grid 
+        container 
+        spacing={4}
+        direction='row'
+        justifyContent='space-between'
+        sx={{
+            my: 10
+        }}
+        >
+            <Grid size={{ xs: 12, md: 6 }}>
                 <Box
-                    sx={{
-                    display: "flex",
-                    flexDirection: "column",
-                    gap: 1,
-                    }}
-                >
-                    {/* Miniaturas (simulada una sola) */}
-                    <Box
-                    component="img"
-                    src={sneaker.image}
-                    alt={sneaker.model}
-                    sx={{
-                        width: 70,
-                        height: 70,
-                        objectFit: "cover",
-                        borderRadius: 1,
-                        border: "1px solid #ccc",
-                    }}
-                    />
-                </Box>
-
-                {/* Imagen principal */}
-                <Box
-                    component="img"
-                    src={sneaker.image}
-                    alt={sneaker.model}
-                    sx={{
+                component="img"
+                src={sneaker.image}
+                alt={sneaker.model}
+                size={{ xs: 12 }}
+                sx={{
                     width: "100%",
                     maxWidth: 550,
                     height: "auto",
                     borderRadius: 2,
-                    ml: 2,
-                    }}
+                }}
                 />
-                </Stack>
+                <Box
+                component="img"
+                src={sneaker.image}
+                alt={sneaker.model}
+                size={{ xs: 12}}
+                sx={{
+                    width: 70,
+                    height: 70,
+                    objectFit: "cover",
+                    borderRadius: 1,
+                    border: "1px solid #ccc",
+                }}
+                />
             </Grid>
 
             {/* Info del producto */}
-            <Grid item xs={12} md={6}>
-                <Typography variant="h3" fontWeight="bold">
+            <Grid size={{ xs: 12, md: 6 }}>
+                <Typography variant="h3" sx={{ fontFamily: "Inter", fontSize: { xs: '30px',md: '45px'}, fontWeight: "bold" }}>
                 {sneaker.model}
                 </Typography>
-                <Typography variant="h5" color="text.secondary" mb={2}>
+                <Typography variant="h5" sx={{ color: "text.secondary", fontFamily: "Inter", fontSize: '20px', my: 2, fontWeight: "bold" }}>
                 {sneaker.brand}
                 </Typography>
 
@@ -98,7 +92,23 @@ return (
                     sx={{ flexWrap: "wrap", gap: 1 }}
                 >
                     {sneaker.sizes.map((size) => (
-                    <ToggleButton key={size} value={size}>
+                    <ToggleButton 
+                    key={size}
+                    value={size}
+                    selected={selectedSize === size}
+                    color="primary"
+                    sx={{
+                        border: "1px solid #ccc",
+                        color: "#000",
+                        '&.Mui-selected': {
+                        backgroundColor: '#1976d2',
+                        color: 'white',
+                        },
+                        '&.Mui-selected:hover': {
+                        backgroundColor: '#1565c0',
+                        }
+                    }}
+                    >
                         {size}
                     </ToggleButton>
                     ))}
@@ -109,7 +119,7 @@ return (
                 <Button
                     variant="contained"
                     color="primary"
-                    sx={{ mt: 3, borderRadius: 999, px: 4 }}
+                    sx={{ my: { xs: 6 }, borderRadius: 999, px: 4 }}
                     onClick={() => {
                         setDialogOpen(true);
                     }}
@@ -119,7 +129,6 @@ return (
                 </Button>
 
                 {/* Descripción */}
-                <Box mt={4}>
                 <Typography variant="h5" fontWeight="bold" gutterBottom>
                     Comodidad y rendimiento
                 </Typography>
@@ -129,120 +138,117 @@ return (
                     suela cómoda, son ideales tanto para el día a día como para
                     ocasiones especiales.
                 </Typography>
-                </Box>
             </Grid>
-            </Grid>
+        </Grid>
 
-            {/* Alerta Confirmar Agregar al Carrito */}
-            <Dialog
-                open={dialogOpen}
-                onClose={() => setDialogOpen(false)}
-                fullWidth
-                maxWidth="xs"
-                PaperProps={{
-                    sx: {
-                    borderRadius: 4,
-                    backgroundColor: "white",
-                    textAlign: "center",
-                    p: 3,
-                    },
+        {/* Alerta Confirmar Agregar al Carrito */}
+        <Dialog
+            open={dialogOpen}
+            onClose={() => setDialogOpen(false)}
+            fullWidth
+            maxWidth="xs"
+            PaperProps={{
+                sx: {
+                borderRadius: 4,
+                backgroundColor: "white",
+                textAlign: "center",
+                p: 3,
+                },
+            }}
+            BackdropProps={{
+                sx: {
+                backgroundColor: "rgba(0, 0, 0, 0.4)", // fondo opaco
+                },
+            }}
+            >
+            <DialogTitle>Producto agregado</DialogTitle>
+            <DialogContent>
+                <Typography>
+                ¿Deseás confirmar agregar el producto <strong>{sneaker.model}</strong> al carrito?
+                </Typography>
+            </DialogContent>
+            <DialogActions sx={{ justifyContent: "center" }}>
+                <Button
+                onClick={() => {
+                    addProduct(sneaker);
+                    setDialogOpen(false);
                 }}
-                BackdropProps={{
-                    sx: {
-                    backgroundColor: "rgba(0, 0, 0, 0.4)", // fondo opaco
-                    },
-                }}
+                variant="contained"
+                color="primary"
+                sx={{ borderRadius: 999, px: 4 }}
                 >
-                <DialogTitle>Producto agregado</DialogTitle>
-                <DialogContent>
-                    <Typography>
-                    ¿Deseás confirmar agregar el producto <strong>{sneaker.model}</strong> al carrito?
-                    </Typography>
-                </DialogContent>
-                <DialogActions sx={{ justifyContent: "center" }}>
-                    <Button
-                    onClick={() => {
-                        addProduct(sneaker);
-                        setDialogOpen(false);
-                    }}
-                    variant="contained"
-                    color="primary"
-                    sx={{ borderRadius: 999, px: 4 }}
-                    >
-                    Aceptar
-                    </Button>
-                </DialogActions>
-            </Dialog>
+                Aceptar
+                </Button>
+            </DialogActions>
+        </Dialog>
 
-            <Box mt={5} width={800}>
-                <Typography variant="h5" gutterBottom>Información adicional</Typography>
+        <Box sx={{ width: "100%" }}>
+            <Typography variant="h5" gutterBottom sx={{ fontFamily: "Inter" }}>Información adicional</Typography>
 
-                <Accordion>
-                    <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                    <Typography variant='h6'>Cambios y devoluciones</Typography>
-                    </AccordionSummary>
-                    <AccordionDetails>
-                    <Typography variant="body1">
-                        Podés cambiar o devolver cualquier producto en un plazo de 30 días, siempre que esté en condiciones originales.
-                    </Typography>
-                    </AccordionDetails>
-                </Accordion>
+            <Accordion>
+                <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography variant='h6' sx={{ fontFamily: "Inter", fontSize: { xs: '15px', md: '18px' },} }>Cambios y devoluciones</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                <Typography variant="body1" sx={{ fontFamily: "Inter", fontSize: { xs: '15px' },}}>
+                    Podés cambiar o devolver cualquier producto en un plazo de 30 días, siempre que esté en condiciones originales.
+                </Typography>
+                </AccordionDetails>
+            </Accordion>
 
-                <Accordion>
-                    <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                    <Typography variant='h6'>Métodos de pago</Typography>
-                    </AccordionSummary>
-                    <AccordionDetails>
-                    <Typography variant="body1">
-                        Aceptamos tarjetas de crédito, débito, MercadoPago y transferencia bancaria.
-                    </Typography>
-                    </AccordionDetails>
-                </Accordion>
+            <Accordion>
+                <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography variant='h6' sx={{ fontFamily: "Inter", fontSize: { xs: '15px', md: '18px' } }}>Métodos de pago</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                <Typography variant="body1" sx={{ fontFamily: "Inter", fontSize: { xs: '15px' },}}>
+                    Aceptamos tarjetas de crédito, débito, MercadoPago y transferencia bancaria.
+                </Typography>
+                </AccordionDetails>
+            </Accordion>
 
-                <Accordion>
-                    <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                    <Typography variant='h6'>Envíos</Typography>
-                    </AccordionSummary>
-                    <AccordionDetails>
-                    <Typography variant="body1">
-                        Envío gratuito a todo el país en compras mayores a $50.000. Despacho en 24 horas.
-                    </Typography>
-                    </AccordionDetails>
-                </Accordion>
-            </Box>
-
-            {/* Productos recomendados */}
-            <Box mt={8}>
-            <Typography variant="h5" mb={2}>
-                Te recomendamos
-            </Typography>
-            <Grid container spacing={2}>
-                {dataDestacados
-                .filter((item) => item.id !== sneaker.id)
-                .slice(0, 3)
-                .map((prod) => (
-                    <Grid item xs={12} sm={4} key={prod.id}>
-                    <Box sx={{ textAlign: "center" }}>
-                        <Box
-                        component="img"
-                        src={prod.image}
-                        alt={prod.model}
-                        sx={{ width: "100%", maxWidth: 250, mb: 1 }}
-                        />
-                        <Typography variant="body1">{prod.model}</Typography>
-                        <Typography variant="body2" color="text.secondary">
-                        ${prod.price}
-                        </Typography>
-                        <Link to={`/producto/${prod.id}`} style={{ textDecoration: "none", color: "inherit" }}>
-                            <Box>Ver Producto</Box>
-                        </Link>
-                    </Box>
-                    </Grid>
-                    
-                ))}
-            </Grid>
-            </Box>
+            <Accordion>
+                <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography variant='h6' sx={{ fontFamily: "Inter", fontSize: { xs: '15px', md: '18px' } }}>Envíos</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                <Typography variant="body1" sx={{ fontFamily: "Inter", fontSize: { xs: '15px' },}}>
+                    Envío gratuito a todo el país en compras mayores a $50.000. Despacho en 24 horas.
+                </Typography>
+                </AccordionDetails>
+            </Accordion>
         </Box>
-    </motion.div>
+
+        {/* Productos recomendados */}
+        <Box sx={{ width: "100%", my: 15 }}>
+        <Typography variant="h5" mb={2}>
+            Te recomendamos
+        </Typography>
+        <Grid container spacing={2}>
+            {dataDestacados
+            .filter((item) => item.id !== sneaker.id)
+            .slice(0, 3)
+            .map((prod) => (
+                <Grid size={{ xs: 12, sm: 6, md: 3 }} key={prod.id}>
+                <Box sx={{ textAlign: "center" }}>
+                    <Box
+                    component="img"
+                    src={prod.image}
+                    alt={prod.model}
+                    sx={{ width: "100%", maxWidth: 250, mb: 1 }}
+                    />
+                    <Typography variant="body1">{prod.model}</Typography>
+                    <Typography variant="body2" color="text.secondary">
+                    ${prod.price}
+                    </Typography>
+                    <Link to={`/producto/${prod.id}`} style={{ textDecoration: "none", color: "inherit" }}>
+                        <Box>Ver Producto</Box>
+                    </Link>
+                </Box>
+                </Grid>
+            ))}
+        </Grid>
+        </Box>
+    </Grid>
 );
 }
