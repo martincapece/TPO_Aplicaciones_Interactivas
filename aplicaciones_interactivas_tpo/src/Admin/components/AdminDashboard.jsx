a2ff69937e1c73dc92fbb54a3fbc9a6b98f6f289
simon forteza <fortezasimon@gmail.com>
2025-05-03 17:37:52 -0300

    feature(admin): uso de la data de destacados

--- aplicaciones_interactivas_tpo/src/Admin/components/AdminDashboard.jsx
+++ aplicaciones_interactivas_tpo/src/Admin/components/AdminDashboard.jsx
@@ -1,129 +1,139 @@
 import * as React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
 import Paper from '@mui/material/Paper';
-import products from '../../mocks/products.json' // importo los products del mock
-import { Avatar } from '@mui/material';
+import { IconButton, Select, MenuItem, Checkbox, Button, Box } from '@mui/material'; // Asegúrate de importar Select y MenuItem
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
-import { IconButton } from '@mui/material';
-import { Select, MenuItem } from '@mui/material';
-import { Button, Box } from '@mui/material';
-import { Navigate, useNavigate } from 'react-router-dom';
-
-
-
-const columns = [
-  { field: 'id', headerName: 'ID', width: 70 , sortable: false, filterable: false, headerAlign: 'center', align: 'center'},
-  {
-    field: 'img',
-    headerName: 'Imagen',
-    width: 130, renderCell: params => (
-      <div style={{ 
-        display: 'flex', 
-        justifyContent: 'center', 
-        alignItems: 'center', 
-        height: '100%'
-      }}>
-        <img
-          src={params.row.img}
-          alt={params.row.name}
-          style={{ width: 76, height: 76, objectFit: 'cover' }}
-        />
-      </div>
-    ),
-    sortable: false,
-    filterable: false,
-    headerAlign: 'center',
-    align: 'center'
-  },
-  { field: 'name', headerName: 'Nombre', width: 200, headerAlign: 'center', align: 'center' },
-  { field: 'brand', headerName: 'Marca', width: 130, headerAlign: 'center', align: 'center' },
-  { field: 'price', headerName: 'Precio', width: 100, type: 'number', headerAlign: 'center', align: 'center' },
-  {
-    field: 'sizes',
-    headerName: 'Tallas',
-    width: 150,
-    renderCell: (params) => {
-      const sizes = Array.isArray(params.row.sizes) ? params.row.sizes : [];
-    
-      return (
-        <Select
-          value={sizes[0] || ''}
-          size="small"
-          fullWidth
-          sx={{ backgroundColor: 'transparent' }}
-        >
-          {sizes.map((size, index) => (
-            <MenuItem key={index} value={size}>
-              {size}
-            </MenuItem>
-          ))}
-        </Select>
-      );
-    },
-    sortable: false,
-    filterable: false,
-    headerAlign: 'center',
-    align: 'center'
-  },
-  { field: 'stock', headerName: 'Stock', width: 100, type: 'number', headerAlign: 'center', align: 'center'},
-  { field: 'category', headerName: 'Categoría', width: 130, headerAlign: 'center', align: 'center' },
-  {
-    field: 'actions',
-    headerName: 'Acciones',
-    width: 150,
-    renderCell: (params) => (
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
-        <IconButton color="primary" onClick={() => handleEdit(params.row.id)}>
-          <EditIcon />
-        </IconButton>
-        <IconButton color="secondary" onClick={() => handleDelete(params.row.id)}>
-          <DeleteIcon />
-        </IconButton>
-      </div>
-    ),
-    sortable: false,
-    filterable: false,
-    headerAlign: 'center',
-    align: 'center'
-  },
-];
-
-const paginationModel = { page: 0, pageSize: 5 };
-
-const handleEdit = (id) => {
-  console.log(`Edit product with id: ${id}`);
-  // Lógica para editar el producto
-};
-
-const handleDelete = (id) => {
-  console.log(`Delete product with id: ${id}`);
-  // Lógica para eliminar el producto
-};
+import { useNavigate } from 'react-router-dom';
+import { dataDestacados } from '../../ecomerce/data/dataDestacados'; // Asegúrate de que este path sea correcto
 
 export default function AdminDashboard() {
   const navigate = useNavigate();
+  const [productRows, setProductRows] = React.useState(
+    dataDestacados.map(producto => ({ ...producto, featured: false })) // Añadir la propiedad 'featured'
+  );
+
+  const handleEdit = (id) => {
+    console.log(`Edit product with id: ${id}`);
+  };
+
+  const handleDelete = (id) => {
+    console.log(`Delete product with id: ${id}`);
+  };
+
+  const columns = [
+    { field: 'id', headerName: 'ID', flex: 0.1, headerAlign: 'center', align: 'center' },
+    {
+      field: 'image',
+      headerName: 'Imagen',
+      flex: 0.2,
+      renderCell: (params) => (
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
+          <img
+            src={params.row.image[0]}
+            alt={params.row.model}
+            style={{ width: 76, height: 76, objectFit: 'cover' }}
+          />
+        </div>
+      ),
+      sortable: false,
+      filterable: false,
+      headerAlign: 'center',
+      align: 'center'
+    },
+    { field: 'model', headerName: 'Modelo', flex: 0.2, headerAlign: 'center', align: 'center' },
+    { field: 'brand', headerName: 'Marca', flex: 0.15, headerAlign: 'center', align: 'center' },
+    { field: 'price', headerName: 'Precio', flex: 0.1, type: 'number', headerAlign: 'center', align: 'center' },
+    {
+      field: 'sizes',
+      headerName: 'Tallas',
+      flex: 0.2,
+      renderCell: (params) => {
+        const sizes = Array.isArray(params.row.sizes) ? params.row.sizes : [];
+        return (
+          <Select
+            value={sizes[0] || ''}
+            size="small"
+            fullWidth
+            sx={{ backgroundColor: 'transparent' }}
+          >
+            {sizes.map((size, index) => (
+              <MenuItem key={index} value={size}>
+                {size}
+              </MenuItem>
+            ))}
+          </Select>
+        );
+      },
+      sortable: false,
+      filterable: false,
+      headerAlign: 'center',
+      align: 'center'
+    },
+    {
+      field: 'featured',
+      headerName: 'Destacado',
+      flex: 0.1,
+      renderCell: (params) => (
+        <Checkbox
+          checked={params.row.featured}
+          onChange={() => {
+            const updatedRows = productRows.map((row) =>
+              row.id === params.row.id ? { ...row, featured: !row.featured } : row
+            );
+            setProductRows(updatedRows);
+          }}
+          color="primary"
+        />
+      ),
+      sortable: false,
+      filterable: false,
+      headerAlign: 'center',
+      align: 'center'
+    },
+    {
+      field: 'actions',
+      headerName: 'Acciones',
+      flex: 0.2,
+      renderCell: (params) => (
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
+          <IconButton color="primary" onClick={() => handleEdit(params.row.id)}>
+            <EditIcon />
+          </IconButton>
+          <IconButton color="secondary" onClick={() => handleDelete(params.row.id)}>
+            <DeleteIcon />
+          </IconButton>
+        </div>
+      ),
+      sortable: false,
+      filterable: false,
+      headerAlign: 'center',
+      align: 'center'
+    }
+  ];
 
   return (
     <>
-    <Box display="flex" justifyContent="space-between" alignItems="center" mb={2} mr={4} mt={2} ml={2}>
-        <h1>DashBoard</h1>
+      <Box display="flex" justifyContent="space-between" alignItems="center" mb={2} mr={4} mt={2} ml={2}>
+        <h1>Dashboard</h1>
         <Button variant="contained" color="primary" onClick={() => navigate('/inicio/admin/new-product')}>
           Agregar producto
         </Button>
-    </Box>
-    <Paper sx={{ height: 500, width: '100%' }}>
-      <DataGrid
-        rows={products}
-        columns={columns}
-        getRowId={row => row.id}
-        initialState={{ pagination: { paginationModel } }}
-        pageSizeOptions={[5, 10]}
-        checkboxSelection
-        rowHeight={80}
-        sx={{ border: 0 }}
-      />
-    </Paper>
+      </Box>
+      <Paper sx={{ height: 500, width: '100%' }}>
+        <DataGrid
+          rows={productRows}
+          columns={columns}
+          getRowId={(row) => row.id}
+          pageSize={5} // Tamaño de página inicial
+          pageSizeOptions={[5, 10, productRows.length]} // Opciones de tamaño de página disponibles
+          checkboxSelection
+          rowHeight={80}
+          disableRowSelectionOnClick
+          sx={{ border: 0 }}
+          pagination
+        />
+      </Paper>
     </>
   );
 }
