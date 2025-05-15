f7447439b8fedc127e45aeccdc476086d96bafde
Martin Capece <macapece@uade.edu.ar>
2025-05-15 16:48:58 -0300

    refactor(admin): codigo mas legible en AdminRoutes

--- aplicaciones_interactivas_tpo/src/Admin/routes/AdminRoutes.jsx
+++ aplicaciones_interactivas_tpo/src/Admin/routes/AdminRoutes.jsx
@@ -6,21 +6,10 @@ import NewProductPage from "../pages/NewProductPage";
 export const AdminRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<AdminLayout />}>
         <Route index element={<AdminPage />} />
         <Route path="new-product" element={<NewProductPage />} />
         <Route path="edit-product/:id" element={<NewProductPage />} />
         <Route path="*" element={<Navigate to="/admin" />} />
-      </Route>
     </Routes>
-  );
-};
-
-const AdminLayout = () => {
-  return (
-    <div>
-      {/* Aquí podrías tener un sidebar, header, etc */}
-      <Outlet />
-    </div>
   );
 };
